cd00e676ee803279518391ab11d157b45362be52
prathamtomar99 <162817709+prathamtomar99@users.noreply.github.com>
2024-12-05 22:48:13 +0530

    Create nn.h

---
+++ deep_learning/nn.h
@@ -0,0 +1,17 @@
+#ifndef NN_H
+#define NN_H
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#endif
+#ifdef NN_IMPLEMENTATION
+#endif
